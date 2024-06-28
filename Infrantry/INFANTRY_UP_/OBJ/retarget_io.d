fa018b7419c782132d565eb3c076380422bc261d
LeeZekai233 <115407059+LeeZekai233@users.noreply.github.com>
2024-06-28 15:58:08 +0800

    kill .o.f

--- Infrantry/INFANTRY_UP_/OBJ/retarget_io.d
+++
@@ -1,8 +0,0 @@
-..\obj\retarget_io.o: T:/SoftWare/Keil5.39/keil_Pack/Keil/ARM_Compiler/1.7.2/Source/retarget_io.c
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\string.h
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\assert.h
-..\obj\retarget_io.o: T:\SoftWare\Keil5.39\keil5.39\ARM\ARM_Compiler_5.06u7\Bin\..\include\rt_sys.h
-..\obj\retarget_io.o: .\RTE\_Template\RTE_Components.h
